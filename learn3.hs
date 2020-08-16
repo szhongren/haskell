56be11c31b4e99b7f6c81f2a3b11d3047f5a8c46
Zhongren Shao <shao.zhongren@gmail.com>
2020-08-16 15:42:27 -0500

    format

--- learn3.hs
+++ learn3.hs
@@ -1,5 +1,7 @@
+-- basic pattern matching
 lucky :: (Integral a) => a -> String
 lucky 7 = "LUCKY NUMBER SEVEN!"
+-- else branch
 lucky x = "Sorry, you're out of luck, pal!"
 
 sayMe :: (Integral a) => a -> String
@@ -30,71 +32,69 @@ third :: (a, b, c) -> c
 third (_, _, z) = z
 
 head' :: [a] -> a
-head' []      = error "can't get head of an empty list"
+head' [] = error "can't get head of an empty list"
 -- match head, and tail
 head' (x : _) = x
 
 tail' :: [a] -> [a]
-tail' []       = error "can't get tail of an empty list"
+tail' [] = error "can't get tail of an empty list"
 tail' (_ : xs) = xs
 
 tell :: (Show a) => [a] -> String
-tell []           = "empty list"
-tell (x     : []) = "one element: " ++ show x
+tell [] = "empty list"
+tell (x : []) = "one element: " ++ show x
 tell (x : y : []) = "two elements: " ++ show x ++ ", " ++ show y
-tell (x : y : _ ) = "long list: " ++ show x ++ ", " ++ show y ++ ", etc."
+tell (x : y : _) = "long list: " ++ show x ++ ", " ++ show y ++ ", etc."
 
 length' :: (Num b) => [a] -> b
-length' []       = 0
+length' [] = 0
 length' (_ : xs) = 1 + length' xs
 
 sum' :: (Num a) => [a] -> a
-sum' []       = 0
+sum' [] = 0
 sum' (x : xs) = x + sum' xs
 
 capital :: String -> String
-capital ""           = "Empty string, whoops!"
+capital "" = "Empty string, whoops!"
+-- as pattern, all matches the entire thing
 capital all@(x : xs) = "The first letter of " ++ all ++ " is " ++ [x]
 
 -- guards are like if statements, but easier to read and work nicely with patterns
 bmiTell :: (RealFloat a) => a -> String
-bmiTell bmi | bmi <= 18.5 = "you are underweight"
-            | bmi <= 25.0 = "you are normal"
-            | bmi <= 30.0 = "you are overweight"
-            | otherwise   = "you are off the scale"
+bmiTell bmi
+  | bmi <= 18.5 = "you are underweight"
+  | bmi <= 25.0 = "you are normal"
+  | bmi <= 30.0 = "you are overweight"
+  | otherwise = "you are off the scale"
 
 bmiTell' :: (RealFloat a) => a -> a -> String
-bmiTell' weight height | weight / height ^ 2 <= 18.5 = "you are underweight"
-                       | weight / height ^ 2 <= 25.0 = "you are normal"
-                       | weight / height ^ 2 <= 30.0 = "you are overweight"
-                       | otherwise                   = "you are off the scale"
+bmiTell' weight height
+  | weight / height ^ 2 <= 18.5 = "you are underweight"
+  | weight / height ^ 2 <= 25.0 = "you are normal"
+  | weight / height ^ 2 <= 30.0 = "you are overweight"
+  | otherwise = "you are off the scale"
 
 max' :: (Ord a) => a -> a -> a
-max' a b | a > b     = a
-         | otherwise = b
+max' a b
+  | a > b = a
+  | otherwise = b
 
 compare' :: (Ord a) => a -> a -> Ordering
-a `compare'` b | a > b     = GT
-               | a == b    = EQ
-               | otherwise = LT
+a `compare'` b
+  | a > b = GT
+  | a == b = EQ
+  | otherwise = LT
 
 -- where
 bmiTell'' :: (RealFloat a) => a -> a -> String
-bmiTell'' weight height | bmi <= skinny = "you are underweight"
-                        | bmi <= normal = "you are normal"
-                        | bmi <= fat    = "you are overweight"
-                        | otherwise     = "you are off the scale"
-  -- any declarations in where are visible in the function
-
-
-
-
-
-
-
-
+bmiTell'' weight height
+  | bmi <= skinny = "you are underweight"
+  | bmi <= normal = "you are normal"
+  | bmi <= fat = "you are overweight"
+  | otherwise = "you are off the scale"
   where
-    bmi                   = weight / height ^ 2
+    -- any declarations in where are visible in the function
+    bmi = weight / height ^ 2
     (skinny, normal, fat) = (18.5, 25.0, 30.0)
 
 initials :: String -> String -> String
@@ -104,93 +104,96 @@ initials firstName lastName = [f] ++ ". " ++ [l] ++ "."
     (l : _) = lastName
 
 calcBmis :: (RealFloat a) => [(a, a)] -> [a]
-calcBmis xs = [ bmi w h | (w, h) <- xs ]
-    where bmi weight height = weight / height ^ 2
+calcBmis xs = [bmi w h | (w, h) <- xs]
+  where
+    bmi weight height = weight / height ^ 2
 
 -- let
 -- let bindings are also expressions, so we can put these anywhere we can put any other expression to be evaluated
 cylinder :: (RealFloat a) => a -> a -> a
 cylinder r h =
-    let sideArea = 2 * pi * r * h
-        topArea  = pi * r ^ 2
-    in  sideArea + 2 * topArea
+  let sideArea = 2 * pi * r * h
+      topArea = pi * r ^ 2
+   in -- in is followed by the expression to evaluate
+      sideArea + 2 * topArea
 
 calcBmis' :: (RealFloat a) => [(a, a)] -> [a]
-calcBmis' xs = [ bmi | (w, h) <- xs, let bmi = w / h ^ 2 ]
+calcBmis' xs = [bmi | (w, h) <- xs, let bmi = w / h ^ 2]
 
 -- case
 head'' :: [a] -> a
 head'' xs = case xs of
-    []      -> error "No head"
-    (x : _) -> x
+  [] -> error "No head"
+  (x : _) -> x
 
 describeList :: [a] -> String
-describeList xs = "The list is " ++ case xs of
-    []  -> "empty."
+describeList xs =
+  "The list is " ++ case xs of
+    [] -> "empty."
     [x] -> "a singleton list."
-    xs  -> "a longer list."
+    xs -> "a longer list."
 
 describeList' :: [a] -> String
 describeList' xs = "The list is " ++ what xs
   where
-    what []  = "empty."
+    what [] = "empty."
     what [x] = "a singleton list."
-    what xs  = "a longer list."
+    what xs = "a longer list."
 
 main = do
-    print [ lucky x | x <- [1 .. 10] ]
-    print [ sayMe x | x <- [1 .. 10] ]
-    print [ factorial x | x <- [1 .. 10] ]
-    print
-        [ (x, y)
-        | (x, y) <-
-            [ addVectors (x1, y1) (x2, y2)
-            | x1 <- [1 .. 3]
-            , y1 <- [1 .. 3]
-            , x2 <- [1 .. 3]
-            , y2 <- [1 .. 3]
-            ]
-        , y + x == 6
-        ]
-    print (head' "ATL")
-    print (tail' [1, 3, 5])
-    print (tell "AB")
-    print (tell "ABCDE")
-    print (length' [1, 3, 5, 7])
-    print (sum' [1, 3, 5, 7])
-    print (capital "Dracula")
+  print [lucky x | x <- [1 .. 10]]
+  print [sayMe x | x <- [1 .. 10]]
+  print [factorial x | x <- [1 .. 10]]
+  print
+    [ (x, y)
+      | (x, y) <-
+          [ addVectors (x1, y1) (x2, y2)
+            | x1 <- [1 .. 3],
+              y1 <- [1 .. 3],
+              x2 <- [1 .. 3],
+              y2 <- [1 .. 3]
+          ],
+        y + x == 6
+    ]
+  print (head' "ATL")
+  print (tail' [1, 3, 5])
+  print (tell "AB")
+  print (tell "ABCDE")
+  print (length' [1, 3, 5, 7])
+  print (sum' [1, 3, 5, 7])
+  print (capital "Dracula")
 
-    -- guards
-    print (bmiTell 23)
-    print (bmiTell' 85 1.9)
-    print (max' 1 2)
-    print (compare' 1 2)
+  -- guards
+  print (bmiTell 23)
+  print (bmiTell' 85 1.9)
+  print (max' 1 2)
+  print (compare' 1 2)
 
-    -- where
-    print (bmiTell'' 85 1.9)
-    print (initials "Darth" "Vader")
-    print (calcBmis [(85, 1.9), (89, 1.9)])
+  -- where
+  print (bmiTell'' 85 1.9)
+  print (initials "Darth" "Vader")
+  print (calcBmis [(85, 1.9), (89, 1.9)])
 
-    -- let
-    print (cylinder 4 5)
-    print (4 * (let a = 9 in a + 1) + 2)
-    print [let square x = x * x in (square 5, square 3, square 2)]
-    print
-        ( let a = 100
-              b = 200
-              c = 300
-          in  a * b * c
-        , let foo = "Hey "
-              bar = "there!"
-          in  foo ++ bar
-        )
-    print ((let (a, b, c) = (1, 2, 3) in a + b + c) * 100)
-    print (calcBmis' [(85, 1.9), (89, 1.9)])
+  -- let
+  print (cylinder 4 5)
+  print (4 * (let a = 9 in a + 1) + 2)
+  print [let square x = x * x in (square 5, square 3, square 2)]
+  print
+    ( let a = 100
+          b = 200
+          c = 300
+       in a * b * c,
+      let foo = "Hey "
+          bar = "there!"
+       in foo ++ bar
+    )
+  print ((let (a, b, c) = (1, 2, 3) in a + b + c) * 100)
+  print (calcBmis' [(85, 1.9), (89, 1.9)])
 
-    -- case
-    print (describeList [])
-    print (describeList' [])
-    print (describeList [1])
-    print (describeList' [1])
-    print (describeList [1, 2])
-    print (describeList' [1, 2])
+  -- case
+  print (describeList [])
+  print (describeList' [])
+  print (describeList [1])
+  print (describeList' [1])
+  print (describeList [1, 2])
+  print (describeList' [1, 2])
--- learn5.hs
+++ learn5.hs
@@ -20,8 +20,8 @@ applyTwice :: (a -> a) -> a -> a
 applyTwice f x = f (f x)
 
 zipWith' :: (a -> b -> c) -> [a] -> [b] -> [c]
-zipWith' _ []       _        = []
-zipWith' _ _        []       = []
+zipWith' _ [] _ = []
+zipWith' _ _ [] = []
 zipWith' f (x : xs) (y : ys) = f x y : zipWith' f xs ys
 
 flip' :: (a -> b -> c) -> (b -> a -> c)
@@ -33,53 +33,74 @@ flip'' f y x = f x y
 quicksort' :: (Ord a) => [a] -> [a]
 quicksort' [] = []
 quicksort' (x : xs) =
-    let smallerSorted = quicksort' (filter (<= x) xs)
-        biggerSorted  = quicksort' (filter (> x) xs)
-    in  smallerSorted ++ [x] ++ biggerSorted
+  let smallerSorted = quicksort' (filter (<= x) xs)
+      biggerSorted = quicksort' (filter (> x) xs)
+   in smallerSorted ++ [x] ++ biggerSorted
 
 largestDivisible :: (Integral a) => a
 largestDivisible = head (filter p [100000, 99999 ..])
-    where p x = x `mod` 3829 == 0
+  where
+    p x = x `mod` 3829 == 0
 
 largestDivisibleBy :: (Integral a) => a -> [a] -> a
 largestDivisibleBy a ls = head (filter p (reverse (quicksort' ls)))
-    where p x = x `mod` a == 0
+  where
+    p x = x `mod` a == 0
 
 chain :: (Integral a) => a -> [a]
 chain 1 = [1]
-chain n | even n = n : chain (n `div` 2)
-        | odd n  = n : chain (n * 3 + 1)
+chain n
+  | even n = n : chain (n `div` 2)
+  | odd n = n : chain (n * 3 + 1)
 
 numLongChains :: Int
-numLongChains = length (filter isLong (map chain [1..100]))
-    where isLong xs = length xs > 15
+numLongChains = length (filter isLong (map chain [1 .. 100]))
+  where
+    isLong xs = length xs > 15
+
+numLongChains' :: Int
+numLongChains' = length (filter (\xs -> length xs > 15) (map chain [1 .. 100]))
+
+addThree :: (Num a) => a -> a -> a -> a
+addThree x y z = x + y + z
+
+addThree' :: (Num a) => a -> a -> a -> a
+addThree' = \x -> \y -> \z -> x + y + z
+
+flip''' :: (a -> b -> c) -> b -> a -> c
+flip''' f = \x y -> f y x
 
 main = do
-    print (multThree 1 2 3)
-    print (multTwoWithNine 2 3)
-    print (multWithEighteen 4)
-    print (compareWithHundred 4)
-    print (divideByTen 6)
-    print (applyTwice (+ 3) 10)
-    print (applyTwice (++ " HAHA") "HEY")
-    print (applyTwice ("HAHA " ++) "HEY")
-    print (applyTwice (multThree 2 2) 9)
-    print (applyTwice (3 :) [1])
-    print (zipWith' (+) [4, 2, 5, 6] [2, 6, 2, 3])
-    print (zipWith' max [6, 3, 2, 1] [7, 3, 1, 5])
-    print
-        (zipWith' (++)
-                  ["foo ", "bar ", "baz "]
-                  ["fighters", "hoppers", "aldrin"]
-        )
-    print (zipWith' (*) (replicate 5 2) [1 ..])
-    print
-        (zipWith' (zipWith' (*))
-                  [[1, 2, 3], [3, 5, 6], [2, 3, 4]]
-                  [[3, 2, 2], [3, 4, 5], [5, 4, 3]]
-        )
-    print (flip' zip [1, 2, 3, 4, 5] "hello")
-    print (zipWith' (flip' div) [2, 2 ..] [10, 8, 6, 4, 2])
-    print (largestDivisibleBy 5 [1 .. 23])
-    print (sum (takeWhile (< 100000) (filter odd (map (^ 2) [1 ..]))))
-    print numLongChains
+  print (multThree 1 2 3)
+  print (multTwoWithNine 2 3)
+  print (multWithEighteen 4)
+  print (compareWithHundred 4)
+  print (divideByTen 6)
+  print (applyTwice (+ 3) 10)
+  print (applyTwice (++ " HAHA") "HEY")
+  print (applyTwice ("HAHA " ++) "HEY")
+  print (applyTwice (multThree 2 2) 9)
+  print (applyTwice (3 :) [1])
+  print (zipWith' (+) [4, 2, 5, 6] [2, 6, 2, 3])
+  print (zipWith' max [6, 3, 2, 1] [7, 3, 1, 5])
+  print
+    ( zipWith'
+        (++)
+        ["foo ", "bar ", "baz "]
+        ["fighters", "hoppers", "aldrin"]
+    )
+  print (zipWith' (*) (replicate 5 2) [1 ..])
+  print
+    ( zipWith'
+        (zipWith' (*))
+        [[1, 2, 3], [3, 5, 6], [2, 3, 4]]
+        [[3, 2, 2], [3, 4, 5], [5, 4, 3]]
+    )
+  print (flip' zip [1, 2, 3, 4, 5] "hello")
+  print (zipWith' (flip' div) [2, 2 ..] [10, 8, 6, 4, 2])
+  print (largestDivisibleBy 5 [1 .. 23])
+  print (sum (takeWhile (< 100000) (filter odd (map (^ 2) [1 ..]))))
+  print numLongChains
+  print numLongChains'
+  print (zipWith (\a b -> (a * 30 + 3) / b) [5, 4, 3, 2, 1] [1, 2, 3, 4, 5])
+  print (map (\(a, b) -> a + b) [(1, 2), (3, 5), (6, 3), (2, 6), (2, 5)])
