c34c6ead9aba2acc76b946d7fddae5bbb2e524ed
Zhongren Shao <shao.zhongren@gmail.com>
2021-05-04 23:39:14 -0500

    commiting LF changes

--- learnYouAHaskell/learn12.hs
+++ learnYouAHaskell/learn12.hs
@@ -1,3 +1,4 @@
+import Control.Monad.State
 import Control.Monad.Writer
 import qualified Data.ByteString.Lazy as B
 import Data.Monoid
@@ -123,6 +124,73 @@ finalCountDown' x = do
   finalCountDown' (x - 1)
   tell (toDiffList [show x])
 
+-- instance Monad ((->) r) where
+--   return x = \_ -> x
+--   h >>= f = \w -> f (h w) w
+
+addStuff :: Int -> Int
+addStuff = do
+  a <- (* 2)
+  b <- (+ 10)
+  return (a + b)
+
+addStuff' :: Int -> Int
+addStuff' x =
+  let a = (* 2) x
+      b = (+ 10) x
+   in a + b
+
+type Stack = [Int]
+
+pop' :: Stack -> (Int, Stack)
+pop' (x : xs) = (x, xs)
+
+push' :: Int -> Stack -> ((), Stack)
+push' a xs = ((), a : xs)
+
+stackManip' :: Stack -> (Int, Stack)
+stackManip' stack =
+  let ((), newStack1) = push' 3 stack
+      (a, newStack2) = pop' newStack1
+   in pop' newStack2
+
+-- newtype State s a = State {runState :: s -> (a, s)}
+
+-- instance Monad (State s) where
+--   return x = State $ \s -> (x, s)
+--   (State h) >>= f = State $ \s ->
+--     let (a, newState) = h s
+--         (State g) = f a
+--      in g newState
+
+pop :: State Stack Int
+pop = state $ \(x : xs) -> (x, xs)
+
+push :: Int -> State Stack ()
+push a = state $ \xs -> ((), a : xs)
+
+stackManip :: State Stack Int
+stackManip = do
+  push 3
+  a <- pop
+  pop
+
+stackStuff :: State Stack ()
+stackStuff = do
+  a <- pop
+  if a == 5
+    then push 5
+    else do
+      push 3
+      push 8
+
+moreStack :: State Stack ()
+moreStack = do
+  a <- stackManip
+  if a == 100
+    then stackStuff
+    else return ()
+
 writerMonad = do
   print (isBigGang' 3)
   print (isBigGang' 30)
@@ -154,7 +222,22 @@ writerMonad = do
   mapM_ putStrLn $ snd $ runWriter (gcdReverse 8 3)
   print (fromDiffList (toDiffList [1, 2, 3, 4] `mappend` toDiffList [1, 2, 3]))
   mapM_ putStrLn . fromDiffList $ snd $ runWriter (gcdReverse' 110 34)
-  -- mapM_ putStrLn . fromDiffList . snd . runWriter  $ finalCountDown' 50000
-  -- mapM_ putStrLn . snd . runWriter  $ finalCountDown 50000
 
-main = writerMonad
+-- mapM_ putStrLn . fromDiffList . snd . runWriter  $ finalCountDown' 50000
+-- mapM_ putStrLn . snd . runWriter  $ finalCountDown 50000
+
+readerMonad = do
+  let f = (* 5)
+  let g = (+ 3)
+  print ((fmap f g) 8)
+  let f = (+) <$> (* 2) <*> (+ 10)
+  print (f 3)
+  print (addStuff 3)
+  print (addStuff' 3)
+
+stateMonad = do
+  print (stackManip' [5, 8, 2, 1])
+  print (runState stackManip [5, 8, 2, 1])
+  print (runState stackStuff [9, 0, 2, 1, 0])
+
+main = stateMonad
